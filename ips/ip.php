6abf86f2913e8c49db21daba7bc8ba06f46cace8
lxw <1844912514@qq.com>
2018-04-12 13:10:35 +0800

    首次提交

---
+++ ips/ip.php
@@ -0,0 +1,9 @@
+<?php  
+  
+include 'IpLocation.php';  
+  
+echo "<pre>";     
+$ip = "47.93.97.127";     
+$iplocation = new IpLocation();     
+$location = $iplocation->getlocation($ip);     
+print_r($location);    
